67eafec20ee780e9dc6f06091adffa4d55caa9ea
NeddX <nedbeargt@gmail.com>
2023-03-09 02:01:29 -0800

    cleanup

--- release_launch_sac.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell "Start-Process -FilePath '.\Kontrol 2 Server\bin\x64\Release\net6.0-windows\win-x64\k2s.exe' ; Set-Location -Path '.\Kontrol 2 Client\bin\x64\Release\net6.0-windows\win-x64\' ; .\k2c.exe" 
-cmd /k
